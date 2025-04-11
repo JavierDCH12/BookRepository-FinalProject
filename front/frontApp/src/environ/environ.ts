cca427c8b2adcb676fce1689b9da984dce97528b
JavierDCH12 <javierdelgadoch95@gmail.com>
2025-04-11 22:41:09 +0200

    key sup

--- front/frontApp/src/environ/environ.ts
+++ front/frontApp/src/environ/environ.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'https://bookfinder.up.railway.app/api/',/*Ruta básica*/
     supabaseUrl: 'https://rorwpelcykogxwqyaopv.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcndwZWxjeWtvZ3h3cXlhb3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNTE4ODcsImV4cCI6MjA1OTgyNzg4N30.abHEeNuVnCHUCshVD6fGnzdZzTIafuCuAYM2HPJGU3w.eyJpc3MiOiJzdXBhYmFz...'
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcndwZWxjeWtvZ3h3cXlhb3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNTE4ODcsImV4cCI6MjA1OTgyNzg4N30.abHEeNuVnCHUCshVD6fGnzdZzTIafuCuAYM2HPJGU3w'
   };
   
