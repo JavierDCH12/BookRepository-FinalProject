c157775d3ab0d518c82f12838a169e176569ae68
JavierDCH12 <javierdelgadoch95@gmail.com>
2025-04-11 19:50:49 +0200

    added supabase info

--- front/frontApp/src/environ/environ.ts
+++ front/frontApp/src/environ/environ.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://bookfinder.up.railway.app/api/' /*Ruta básica*/
+    apiUrl: 'https://bookfinder.up.railway.app/api/',/*Ruta básica*/
+    supabaseUrl: 'https://rorwpelcykogxwqyaopv.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcndwZWxjeWtvZ3h3cXlhb3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNTE4ODcsImV4cCI6MjA1OTgyNzg4N30.abHEeNuVnCHUCshVD6fGnzdZzTIafuCuAYM2HPJGU3w.eyJpc3MiOiJzdXBhYmFz...'
   };
   
--- front/frontApp/src/supabase/supaBaseClient.ts
+++ front/frontApp/src/supabase/supaBaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import { environment } from '../environ/environ';
 
-const supabaseurl ='https://rorwpelcykogxwqyaopv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcndwZWxjeWtvZ3h3cXlhb3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNTE4ODcsImV4cCI6MjA1OTgyNzg4N30.abHEeNuVnCHUCshVD6fGnzdZzTIafuCuAYM2HPJGU3w';
-
-export const supabase = createClient(supabaseurl, supabaseKey);
+export const supabase = createClient(
+  environment.supabaseUrl,
+  environment.supabaseKey
+);
