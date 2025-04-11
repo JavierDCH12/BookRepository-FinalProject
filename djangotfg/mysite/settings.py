ffc6558c988735a37562d70bffc90798e02a52df
JavierDCH12 <javierdelgadoch95@gmail.com>
2025-04-11 17:11:20 +0200

    migrations done

--- djangotfg/mysite/settings.py
+++ djangotfg/mysite/settings.py
@@ -82,7 +82,7 @@ TEMPLATES = [
 # DATABASE
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_PUBLIC_URL"),
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
