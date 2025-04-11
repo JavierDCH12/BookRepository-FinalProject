ba1f8083df8e6b4a43f0b395aaea03d05a3df6ac
JavierDCH12 <javierdelgadoch95@gmail.com>
2025-04-12 00:29:15 +0200

    added search

--- front/frontApp/src/app/components/book-search/book-search.component.ts
+++ front/frontApp/src/app/components/book-search/book-search.component.ts
@@ -53,7 +53,10 @@ export class BookSearchComponent implements OnInit {
       this.loadFavorites();
       this.loadWishlist();
     }
+  
+    this.onSearch(); 
   }
+  
 
   private transformToBookDTO(book: Book): Partial<FavoriteBook & WishlistBook> {
     return {
