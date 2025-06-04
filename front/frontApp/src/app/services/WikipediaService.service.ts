327c5b3394ac629d6f36b21ab3810d05580744a8
JavierDCH12 <114759236+JavierDCH12@users.noreply.github.com>
2025-06-04 15:54:17 +0200

    Update WikipediaService.service.ts

--- front/frontApp/src/app/services/WikipediaService.service.ts
+++ front/frontApp/src/app/services/WikipediaService.service.ts
@@ -1,37 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
+import { environment } from '../../environ/environ'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class WikipediaService {
-  private wikipediaApiUrl = 'https://en.wikipedia.org/w/api.php';
+  private backendWikipediaUrl = `${environment.apiUrl}wikipedia-link`; 
 
   constructor(private http: HttpClient) {}
 
-  // Obtener enlace de Wikipedia de un autor concreto
   getWikipediaLink(author: string): Observable<string | null> {
-    const params = {
-      action: 'query',
-      format: 'json',
-      origin: '*',
-      titles: author,
-      prop: 'pageprops'
-    };
-  
-    return this.http.get<any>(this.wikipediaApiUrl, { params }).pipe(
-      map(response => {
-        const pages = response.query?.pages;
-        if (!pages) return null;
-  
-        const pageId = Object.keys(pages)[0];
-        if (pageId === '-1') return null; 
-  
-        const formattedTitle = encodeURIComponent(author.replace(/ /g, '_'));
-        return `https://en.wikipedia.org/wiki/${formattedTitle}`;
-      })
-    );
+    const encodedAuthor = encodeURIComponent(author);
+    return this.http
+      .get<{ link: string | null }>(`${this.backendWikipediaUrl}?author=${encodedAuthor}`)
+      .pipe(map(response => response.link));
   }
-  
 }
